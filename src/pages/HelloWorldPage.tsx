fbb8b3672cfaef3a285cfc951449ceb1e506d90e
keyaru77 <whyudacok@gmail.com>
2024-07-06 04:10:47 +0700

    Create HelloWorldPage.tsx

---
+++ src/pages/HelloWorldPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HelloWorldPage: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4 text-white">
+      <h1 className="text-4xl font-bold text-center mb-8">Hello World</h1>
+    </div>
+  );
+};
+
+export default HelloWorldPage;

6129b3c1277d25b8a3c06bfcfeb4775a223f9bf1
keyaru77 <whyudacok@gmail.com>
2024-07-06 03:17:56 +0700

    Update apiService.ts

--- src/apiService.ts
+++ src/apiService.ts
@@ -1,13 +1,23 @@
 import axios from 'axios';
 
-const API_URL = 'https://api.koranime.fun/v1/home';
+const BASE_URL = 'https://api.koranime.fun/v1';
 
 export const fetchHomeData = async () => {
   try {
-    const response = await axios.get(API_URL);
+    const response = await axios.get(`${BASE_URL}/home`);
     return response.data;
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error('Error fetching home data:', error);
+    throw error;
+  }
+};
+
+export const fetchWatchData = async (endpoint: string) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/nonton/${endpoint}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching watch data:', error);
     throw error;
   }
 };

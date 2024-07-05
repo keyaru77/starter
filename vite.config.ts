2b5c512c93f7b8099f7f78c37509d25ad96d2196
keyaru77 <whyudacok@gmail.com>
2024-07-06 04:15:52 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 import VitePluginHtmlEnv from 'vite-plugin-html-env';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svgr(), react(), VitePluginHtmlEnv()],
+  plugins: [svgr(), react(), VitePluginHtmlEnv()],
+  server: {
+    historyApiFallback: true, // Tambahkan ini untuk mendukung SPA routing
+  },
 });
